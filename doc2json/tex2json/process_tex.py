fea41907b3d880323120faeb4b0459157fed4f2d
flychen59 <101077242+flychen59@users.noreply.github.com>
2024-07-21 16:59:57 +0800

    Update process_tex.py

--- doc2json/tex2json/process_tex.py
+++ doc2json/tex2json/process_tex.py
@@ -6,6 +6,16 @@ from typing import Optional, Dict
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..','..')))
 from doc2json.tex2json.tex_to_xml import convert_latex_to_s2orc_json
 from doc2json.tex2json.xml_to_json import convert_latex_xml_to_s2orc_json
+import json
+import io
+import copy
+import os
+from PIL import Image
+import pandas as pd
+from pdf2image import convert_from_path
+import pyarrow as pa
+import pyarrow.parquet as pq
+from collections import OrderedDict
  
 
 
@@ -97,7 +107,104 @@ def process_tex_file(
     with open(output_file, 'w') as outf:
         json.dump(paper.release_json("latex"), outf, indent=4, sort_keys=False)
 
-    return output_file
+    return output_file,output_file
+
+
+
+def read_image(image_path):
+    # 打开图像文件
+    if image_path.lower().endswith('.pdf'):
+                images = convert_from_path(image_path)
+                new_image_path = os.path.splitext(image_path)[0] + ".png"
+                images[0].save(new_image_path, 'PNG')
+                image_path = new_image_path
+    with open(image_path, 'rb') as file:
+        image = Image.open(file)
+        # 将图像转换为二进制格式
+        img_byte_arr = io.BytesIO()
+        image.save(img_byte_arr, format=image.format)
+        img_byte_arr = img_byte_arr.getvalue()
+    return img_byte_arr
+
+
+def save_to_parquet(data, output_path):
+    df = pd.DataFrame(data)
+    table = pa.Table.from_pandas(df)
+    pq.write_table(table, output_path)
+
+def convert_to_target_format_cyp(data, template):
+    result=[]
+    template["文件id"] = data['paper_id']
+    template["处理时间"] = data["header"]["date_generated"]
+    
+    ## step 1 bod_text map to section
+    doc_parse = data["latex_parse"] # todo other doc types
+    body_text = doc_parse["body_text"]
+    body_dict = OrderedDict() # key: section, value: text
+    for text_dict in body_text:
+        if text_dict["section"] in body_dict:
+            body_dict[text_dict["section"]].append(text_dict) # keep all info
+        else:
+            body_dict[text_dict["section"]] = [text_dict] # keep all info
+    
+    ## step 2 build list
+    new_entry = copy.deepcopy(template)
+    new_entry["块id"] = 'title'
+    new_entry["文本"] = data["title"]
+    new_entry["数据类型"] = 'text' 
+    result.append(new_entry)
+    
+    new_entry = copy.deepcopy(template)
+    new_entry["数据类型"] = 'text' 
+    new_entry["块id"] = "abstract"
+    new_entry["文本"] = data["abstract"]
+    result.append(new_entry)
+    
+    
+    ref_entries = doc_parse["ref_entries"]
+    for section, para_list in body_dict.items():
+        template["块id"] = section
+        for para in para_list:     
+            new_entry = copy.deepcopy(template)
+            new_entry["文本"] = para['text'] 
+            new_entry["数据类型"]='text'
+            result.append(copy.deepcopy(new_entry))
+            for ref in para["ref_spans"]:
+                if "ref_id" in ref and ref["ref_id"] in ref_entries:  
+                    new_entry = copy.deepcopy(template)
+                    new_entry["数据类型"] =ref_entries[ref["ref_id"]]['type_str']
+                    new_entry["块id"] = section
+                    if new_entry["数据类型"]=='figure':
+                        path=os.path.join('s2orc-doc2json/temp_dir/latex',data['paper_id'],"".join(ref_entries[ref["ref_id"]]["uris"]))
+                        # path=os.path.join('./temp_dir/latex',data['paper_id'],"".join(ref_entries[ref["ref_id"]]["uris"]))
+                        new_entry["图片"]=read_image(path)   
+                    new_entry["文本"] = ref_entries[ref["ref_id"]]['text']
+                    filtered_entries = {k: v for k, v in ref_entries[ref["ref_id"]].items() if k != 'text' and 'ref_id' }
+                    new_entry["额外信息"] = filtered_entries 
+                    result.append(copy.deepcopy(new_entry))
+               
+            
+            for ref in para["cite_spans"]:
+                if ref["ref_id"] in ref_entries:  
+                    new_entry = copy.deepcopy(template)
+                    new_entry["数据类型"] =ref_entries[ref["ref_id"]]['type_str']   
+                    new_entry["块id"] = section
+                    new_entry["文本"] = ref_entries[ref["ref_id"]]['text'] 
+                    filtered_entries = {k: v for k, v in ref_entries[ref["ref_id"]].items() if k != 'text' and 'ref_id' }
+                    new_entry["额外信息"] = filtered_entries
+                    result.append(copy.deepcopy(new_entry))
+            
+            for ref in para["eq_spans"]:
+                if ref["ref_id"] in ref_entries:  
+                    new_entry = copy.deepcopy(template)
+                    new_entry["数据类型"] =ref_entries[ref["ref_id"]]['type_str']   
+                    new_entry["块id"] = section
+                    new_entry["文本"] = ref_entries[ref["ref_id"]]['text'] 
+                    filtered_entries = {k: v for k, v in ref_entries[ref["ref_id"]].items() if k != 'text' and 'ref_id' }
+                    new_entry["额外信息"] = filtered_entries
+                    result.append(copy.deepcopy(new_entry))
+    
+    return result
 
 
 if __name__ == '__main__':
@@ -106,7 +213,7 @@ if __name__ == '__main__':
     parser.add_argument("-t", "--temp", default='temp', help="path to a temp dir for partial files")
     parser.add_argument("-o", "--output", default='output', help="path to the output dir for putting json files")
     parser.add_argument("-l", "--log", default='log', help="path to the log dir")
-    parser.add_argument("-k", "--keep", default=False, help="keep temporary files")
+    parser.add_argument("-k", "--keep", default=True, help="keep temporary files")
 
     args = parser.parse_args()
 
@@ -121,8 +228,19 @@ if __name__ == '__main__':
     os.makedirs(temp_path, exist_ok=True)
     os.makedirs(output_path, exist_ok=True)
 
-    process_tex_file(input_path, temp_path, output_path, log_path, keep_temp)
+    _,output_file=process_tex_file(input_path, temp_path, output_path, log_path, keep_temp)
+  
 
     runtime = round(time.time() - start_time, 3)
+  
+    
+    template = {"文件md5": None, "文件id": None, "页码": None, "块id": None, "文本": None, "图片": None, "处理时间": None, "数据类型": None, "bounding_box": None, "额外信息": None}
+    # json_path = '/root/autodl-tmp/s2orc-doc2json/output_dir/2004.14974.json'
+    with open(output_file, 'r') as file:
+        data = json.load(file)
+        result = convert_to_target_format_cyp(data, template)
+        
+    output_json_path = os.path.splitext(output_file)[0] + ".parquet"
+    save_to_parquet(result, output_json_path)
     print("runtime: %s seconds " % (runtime))
     print('done.')
